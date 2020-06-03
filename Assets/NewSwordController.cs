c28db5f2b1e17a6d607ef8c1fc35663e60499597
TheWize <aharoniamirr@gmail.com>
2020-06-03 12:12:08 +0300

    irrelevant

---
+++ Assets/NewSwordController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class NewSwordController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float tilt;
+    private float pan;
+    Transform targetTilt;
+    Transform targetPan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tilt = Input.GetAxis("Mouse Y");
+        pan = Input.GetAxis("Mouse X");
+    }
+}
